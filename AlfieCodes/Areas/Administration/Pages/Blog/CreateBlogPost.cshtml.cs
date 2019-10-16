c74701c65ca0c45260d6948842dbd52cd6039b5b
Alfie Gibbs <alfie.gibbs@razor.co.uk>
2019-10-16 11:27:07 +0100

    Added humanizer on the date times.

---
+++ AlfieCodes/Areas/Administration/Pages/Blog/CreateBlogPost.cshtml.cs
@@ -0,0 +1,47 @@
+﻿namespace AlfieCodes.Areas.Administration.Pages.Blog
+{
+    using System;
+    using System.Threading.Tasks;
+    using AlfieCodes.Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class CreateBlogPostModel : PageModel
+    {
+        private readonly BlogDbContext _blogDbContext;
+
+        public CreateBlogPostModel( BlogDbContext blogDbContext )
+        {
+            _blogDbContext = blogDbContext;
+        }
+
+        [ BindProperty ]
+        public BlogPost BlogPost { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if ( !ModelState.IsValid )
+            {
+                return Page();
+            }
+
+            _blogDbContext.BlogPosts.Add( new BlogPost
+            {
+                CreatedAt = DateTime.Now,
+                Title = BlogPost.Title,
+                Body = BlogPost.Body,
+                Summary = BlogPost.Summary,
+                Tags = BlogPost.Tags
+            } );
+
+            await _blogDbContext.SaveChangesAsync();
+
+            return RedirectToPage( "/Index" );
+        }
+    }
+}
